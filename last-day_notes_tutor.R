520225be23b4d03b5b347675601d1323930d288c
kwakuemma1 <emmayeboah2007@gmail.com>
2023-07-10 08:45:51 -0500

    intial commit setting things up

---
+++ last-day_notes_tutor.R
@@ -0,0 +1,14 @@
+# To check for packages
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg) #, dependencies = TRUE
+  sapply(pkg, require, character.only = TRUE)
+}
+
+ipak("all.primer.tutorials")
+
+# Tutorial Packages
+library(r4ds.tutorials)
+library(all.primer.tutorials) 
+library(tidycensus.tutorials)
